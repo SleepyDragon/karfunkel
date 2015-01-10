91005a7a00626cd777b49bca2cb482dd5307722b
Lucas Dohmen <lucas@dohmen.io>
2015-01-10 16:47:57 +0100

    Bug Fix: Creates user even if passwords did not match

--- app.rb
+++ app.rb
@@ -90,10 +90,12 @@ Cuba.define do
       error_on['password_repeat'] = t('register.errors.passwords_not_equal') unless password == password_repeat
       error_on['password'] = t('register.errors.password_too_short') unless password.length > 5
 
-      begin
-        User.create(email: email, password: password, nickname: nickname)
-      rescue Ohm::UniqueIndexViolation
-        error_on['email'] = t('register.errors.email_already_taken')
+      if error_on.empty?
+        begin
+          User.create(email: email, password: password, nickname: nickname)
+        rescue Ohm::UniqueIndexViolation
+          error_on['email'] = t('register.errors.email_already_taken')
+        end
       end
 
       if error_on.empty?
--- test/routes/register_test.rb
+++ test/routes/register_test.rb
@@ -12,4 +12,22 @@ scope do
     assert_equal current_path, '/'
     assert has_content?('lagavulin')
   end
+
+  test 'registering with non matching passwords should not create the user' do
+    visit '/register'
+    fill_in 'email', with: 'oban@whiskey.de'
+    fill_in 'nickname', with: 'oban'
+    fill_in 'password', with: 'ilikewhiskey'
+    fill_in 'password_repeat', with: 'wrongrepeat'
+    click_button 'Registrieren'
+
+    fill_in 'email', with: 'oban@whiskey.de'
+    fill_in 'nickname', with: 'oban'
+    fill_in 'password', with: 'ilikewhiskey'
+    fill_in 'password_repeat', with: 'ilikewhiskey'
+    click_button 'Registrieren'
+
+    assert_equal current_path, '/'
+    assert has_content?('oban')
+  end
 end
