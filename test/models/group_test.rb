d230c28fbefebed2223394c1c154fd868fda5472
Lucas Dohmen <lucas@dohmen.io>
2015-01-11 01:46:02 +0100

    Adjust group model test to take the slug of the system

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -5,7 +5,7 @@ setup do
 end
 
 test 'The group can be saved and retrieved' do
-  created_group = Group.create(name: 'Peergroup', system: 'DSA 5', game_master: @game_master)
+  created_group = Group.create(name: 'Peergroup', system: 'dsa5', game_master: @game_master)
   found_group = Group[created_group.id]
   assert_equal created_group, found_group
   assert_equal found_group.game_master, @game_master
