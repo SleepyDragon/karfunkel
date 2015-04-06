94cecedfa1f5d0cc1a35f241d16b5ddea23d71dc
Lucas Dohmen <lucas@dohmen.io>
2015-04-06 17:13:51 +0000

    Cleanup whitespace

--- app.rb
+++ app.rb
@@ -29,13 +29,13 @@ Cuba.use(Rack::Protection, except: :http_origin)
 Cuba.use(Rack::Protection::RemoteReferrer)
 Cuba.use(Shield::Middleware, "/login")
 
-Dir["./lib/**/*.rb"].each      { |f| require(f) }
-Dir["./models/**/*.rb"].each   { |f| require(f) }
+Dir["./lib/**/*.rb"].each { |f| require(f) }
+Dir["./models/**/*.rb"].each { |f| require(f) }
 Dir["./validations/**/*.rb"].each { |f| require(f) }
-Dir["./filters/**/*.rb"].each  { |f| require(f) }
+Dir["./filters/**/*.rb"].each { |f| require(f) }
 Dir["./services/**/*.rb"].each { |f| require(f) }
-Dir["./helpers/**/*.rb"].each  { |f| require(f) }
-Dir["./routes/**/*.rb"].each   { |f| require(f) }
+Dir["./helpers/**/*.rb"].each { |f| require(f) }
+Dir["./routes/**/*.rb"].each { |f| require(f) }
 
 Cuba.plugin Shield::Helpers
 Cuba.plugin TranslationsHelper
@@ -43,10 +43,10 @@ Cuba.plugin SessionHelper
 Cuba.plugin GroupHelper
 
 Cuba.define do
-  on(root)       { res.redirect '/groups' }
-  on('login')    { run LoginRoutes       }
-  on('logout')   { run LogoutRoutes      }
-  on('register') { run RegisterRoutes    }
+  on(root) { res.redirect '/groups' }
+  on('login') { run LoginRoutes }
+  on('logout') { run LogoutRoutes }
+  on('register') { run RegisterRoutes }
 
   on('groups/(\\d+)') do |group_id|
     group = Group[group_id]
@@ -57,5 +57,5 @@ Cuba.define do
     end
   end
 
-  on('groups')   { run GroupsRoutes      }
+  on('groups') { run GroupsRoutes }
 end
